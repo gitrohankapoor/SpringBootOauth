f38788a13da77c45c9407b33151372235eb1d279
F5399505 <Firstrand$2021>
2022-08-05 09:18:36 +0200

    intital commit with springboot framework dependency

---
+++ src/test/java/com/oauth/oauthdemo/OauthdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.oauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
